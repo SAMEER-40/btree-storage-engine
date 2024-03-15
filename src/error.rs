d08761dc6a67a694d1fefbec4cf857ca577d22e6
Sameer <psam6175@gmail.com>
2024-03-15 09:30:00 +0530

    Initial project scaffold with error types

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Error {
+    Io(std::io::Error),
+    PageNotFound(u32),
+    KeyNotFound,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::Io(e) => write!(f, "I/O error: {}", e),
+            Error::PageNotFound(id) => write!(f, "Page {} not found", id),
+            Error::KeyNotFound => write!(f, "Key not found"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::Io(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Error::Io(e)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("StorageDB - Database Storage Engine");
+}
